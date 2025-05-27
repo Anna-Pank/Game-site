2789e8381e3bac493bf33ef15847c4fa80f59bd5
Anna-Pank <annapank95@gmail.com>
2025-05-27 18:54:57 +0300

    создала мобильную версию

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-// let a = 20;
-// alert(a);
+let a = 20;
+alert(a);
 
-// let b = 2007;
-// alert(b);
+let b = 2007;
+alert(b);
 
-// let nameJS = 'Брендан Эйх (Brendan Eich)'
-// alert(nameJS);
+let nameJS = 'Брендан Эйх (Brendan Eich)'
+alert(nameJS);
 
-// let c = 10;
-// let d = 2;
-// alert(c+d);
-// alert(c-d);
-// alert(c*d);
-// alert(c/d);
+let c = 10;
+let d = 2;
+alert(c+d);
+alert(c-d);
+alert(c*d);
+alert(c/d);
 
-// let e = 2;
-// let power = e ** 5;
-// alert(power);
+let e = 2;
+let power = e ** 5;
+alert(power);
 
-// let f = 9;
-// let g = 2;
-// let result = f % g;
-// alert(result);
+let f = 9;
+let g = 2;
+let result = f % g;
+alert(result);
 
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num++;
-// num--;
-// alert(num);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
 
-// let age = Number(prompt('Сколько вам лет?'));
-// alert(`Вам ${age} лет`);
+let age = Number(prompt('Сколько вам лет?'));
+alert(`Вам ${age} лет`);
 
-// const user = {
-//     name: 'kosha',
-//     age: 3,
-//     isAdmin: true
-// };
+const user = {
+    name: 'kosha',
+    age: 3,
+    isAdmin: true
+};
 
-// let name = String(prompt('Как Вас зовут?'));
-// alert(`Привет, ${name}!`);
+let name = String(prompt('Как Вас зовут?'));
+alert(`Привет, ${name}!`);
